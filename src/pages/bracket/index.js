3f4c1f72898cc22535b8c81c5ee972dec8aa54d2
fachry2 <hckraza@gmail.com>
2020-03-14 07:57:41 +0700

    perbaikan bracket

--- src/pages/bracket/index.js
+++ src/pages/bracket/index.js
@@ -83,8 +83,7 @@ class Test extends React.PureComponent {
   constructor (props) {
     super(props)
     this.state = {
-      match: 16,
-      round: 5
+      match: 128,
     }
   }
   componentDidMount () {
@@ -95,144 +94,206 @@ class Test extends React.PureComponent {
     let g = []
     let X = 244
     let Y = 54
-    const { match, round } = this.state
-
-    for (let r = 0; r < round; r++) {
-      for(let i = r; i < (match * 2) - r; i++) {
-        if (i % 2 === 1) {
-          g.push(
-            GBracket({
-              x: (r*X),
-              y: (i*Y),
-              match: i+1,
-              index: i
-            })
-          )
+    const { match } = this.state
+    const maxRound = Math.log2(match)
+    let arr = []
+    for(let i=0; i< maxRound; i++){
+      if(i===0){
+        arr[i]=[]
+        for(let j=0; j< match; j++){
+          if (j%2===1) {
+              arr[i].push(j)
+              g.push(
+                GBracket({
+                  x: (i*X),
+                  y: (j*Y),
+                  match: j+1,
+                  index: `${i}${j}`
+                })
+              )
+          }
         }
-        if (i % 2 === 0) {
-          g.push(
-            GBracket({
-              x: (r*X),
-              y: (i*Y),
-              match: i+1,
-              index: i
-            })
-          )
+      }
+      if(arr[i].length && i+1 < maxRound){
+        arr[i+1]=[]
+        for(let k=0; k< arr[i].length; k++){
+        const data = arr[i]
+        if(k%2===0){
+          let adder = Math.pow(2, i)
+          if(i+2 === maxRound) {
+            let hasil = data[1]-data[0]
+            arr[i+1].push(hasil)
+            g.push(
+              GBracket({
+                x: ((i+1)*X),
+                y: (hasil*Y),
+                match: hasil+1,
+                index: `${i+1}${hasil}`
+              })
+            )
+          } else {
+            let hasil = data[k]+adder
+            hasil = hasil%2===1 ? hasil+1 : hasil
+            arr[i+1].push(hasil)
+            g.push(
+              GBracket({
+                x: ((i+1)*X),
+                y: (hasil*Y),
+                match: hasil+1,
+                index: `${i+1}${hasil}`
+              })
+            )
+          }
+        }
         }
       }
     }
-
-    // for(let i = 0; i < match; i++) {
-    //   g.push(
-    //     GBracket({
-    //       x: 0,
-    //       y: (i*108),
-    //       match: i+1,
-    //       index: i
-    //     })
-    //   )
-    // }
-
-    // for(let i = 0; i < match*2; i++) {
-    //   if (i === 1 || i%4 === 1) {
-    //     g.push(
-    //       GBracket({
-    //         x: 244,
-    //         y: (i*54),
-    //         match: i+1,
-    //         index: i
-    //       })
-    //     )
-    //   }
-    // }
-
-    // for(let i = 0; i < match*2; i++) {
-    //   if (i === 3 || i%8 === 3) {
-    //     g.push(
-    //       GBracket({
-    //         x: 488,
-    //         y: (i*54),
-    //         match: i+1,
-    //         index: i
-    //       })
-    //     )
-    //   }
-    // }
-
-    // for(let i = 0; i < match*2; i++) {
-    //   if (i === 7) {
-    //     g.push(
-    //       GBracket({
-    //         x: 732,
-    //         y: (54*(i)),
-    //         match: i+1,
-    //         index: i
-    //       })
-    //     )
-    //   }
-    // }
-
+    console.log(arr)
     return g
   }
 
   connector () {
     let c = []
-    const { match } = this.state
-    let PCount = match * 2
-    for(let i=0; i<PCount; i++){
-      const underE = i=== PCount-1 && PCount === 8
-      if(PCount<=8) {
-        if(i === 0 || underE){
-          i = underE? i+1 : i
-          const a = (i+1) * 27
-          const b = (i+3) * 27
-          console.log(a,b)
-          c.push(
-            GConnector({
-              x: 0,
-              y: a,
-              index: i,
-              d: "M 228 1 L 236 1 L 236 54"
-            })
-          )
-          c.push(
-            GConnector({
-              x: 0,
-              y: b,
-              index: i,
-              d: "M 228 53 L 236 53 L 236 1 L 244 1"
-            })
-          )
+    let arrC = []
+    let { match } = this.state
+    const maxRound = Math.log2(match)
+    for(let r = 0, length2 = maxRound; r < length2; r++){
+      if (r===0) {
+        arrC[r] =[]
+        for(let i=0; i< match; i++){
+          const underE = i=== match-1 && match === 8
+          if(match<=8) {
+            if(i === 0 || underE){
+              i = underE? i+1 : i
+              const adder = 3
+              const a = ((i + adder)*27)
+              const b = ((i+ 2 + adder)*27)
+              // console.log(a,b)
+              arrC[r].push(a,b)
+              c.push(
+                GConnector({
+                  x: 0,
+                  y: a,
+                  index: `${a}${b}`,
+                  d: "M 228 1 L 236 1 L 236 54"
+                })
+              )
+              c.push(
+                GConnector({
+                  x: 0,
+                  y: b,
+                  index: `${b}${a}`,
+                  d: "M 228 53 L 236 53 L 236 1 L 244 1"
+                })
+              )
+            }
+          } else {
+            if (i === 0 || (i+1)%4 === 1){
+              const adder = 3
+              const a = ((i*2 + adder)*27)
+              const b = ((i*2+ 2 + adder)*27)
+              // console.log(a,b)
+              arrC[r].push(a,b)
+              c.push(
+                GConnector({
+                  x: 0,
+                  y: a,
+                  index: `${a}${b}`,
+                  d: "M 228 1 L 236 1 L 236 54"
+                })
+              )
+              c.push(
+                GConnector({
+                  x: 0,
+                  y: b,
+                  index: `${b}${a}`,
+                  d: "M 228 53 L 236 53 L 236 1 L 244 1"
+                })
+              )
+            }
+          }
         }
-      } else {
-        if (i === 0 || (i+1)%4 === 1){
-          const a = (i+1) * 27
-          const b = (i+2) * 27
-          console.log(a,b)
-          c.push(
-            GConnector({
-              x: 0,
-              y: a,
-              index: i,
-              d: "M 228 1 L 236 1 L 236 54"
-            })
-          )
-          c.push(
-            GConnector({
-              x: 0,
-              y: b,
-              index: i,
-              d: "M 228 53 L 236 53 L 236 1 L 244 1"
-            })
-          )
+      }
+      if (arrC[r].length && r+1 < maxRound) {
+        arrC[r+1] = []
+        // console.log('r', r)
+        for(var i = 0, length3 = arrC[r].length; i < length3; i++){
+          const data = arrC[r]
+          if ((i%4===1 || i%4===0) && r === 0) {
+            const adder = i%4===0 ? 27 : 54
+            const hasil = data[i]+adder
+            let a, b
+            if (i%4===0) {
+              a = hasil+27
+             // console.log('A', (r+1)*244,a, i, hasil)
+             c.push(
+               GConnector({
+                 x: (r+1)*244,
+                 y: a,
+                 index: `${(r+1)*244}${a}`,
+                 d: `M 228 1 L 236 1 L 236 ${54+((r+1) * 54)}`
+               })
+             )
+            } else {
+              b = hasil+54
+               // console.log('B', (r+1)*244,b, i, hasil)
+               c.push(
+                 GConnector({
+                   x: (r+1)*244,
+                   y: b,
+                   index: `${(r+1)*244}${b}`,
+                   d: `M 228 ${53+((r+1) * 54)} L 236 ${53+((r+1) * 54)} L 236 1 L 244 1`
+                 })
+               )
+            }
+            arrC[r+1].push(hasil)
+          }
+          // samakan aja pola nya, dgn yg bracket utk dpatin nilai nya
+          if ( r >= 1) {
+            if (i< length3/2) {
+            const hasil = data[i]*2
+            // console.log(i, hasil/27, hasil, i%2===0)
+            arrC[r+1].push(hasil)
+             let a, b
+             if (i%2===0) {
+               a = hasil+27
+              // console.log('A', (r+1)*244,a, i, hasil)
+              c.push(
+                GConnector({
+                  x: (r+1)*244,
+                  y: a,
+                  index: `${(r+1)*244}${a}`,
+                  d: `M 228 1 L 236 1 L 236 ${54+((r+1) * 54)+54}`
+                })
+              )
+             } else {
+              const rPlus = r+1
+              const adder = r % 2 ===1 ? r*3 : (r*2)+1
+               b = hasil + (adder*27)
+              console.log('rPlus', r, adder, hasil, b)
+                // console.log('B', rPlus*244,b, i, hasil)
+                c.push(
+                  GConnector({
+                    x: rPlus*244,
+                    y: b,
+                    index: `${rPlus*244}${b}`,
+                    d: `M 228 ${53+(rPlus * 54)+54} L 236 ${53+(rPlus * 54)+54} L 236 1 L 244 1`
+                  })
+                )
+             }
+           // console.log('datax', (r+1)*244,hasil)
+          }
+          }
         }
       }
     }
+    console.log('arrC', arrC)
     return c
   }
 
   render() {
-    const { match, round } = this.state
+    const { match } = this.state
+    const round = Math.log2(match)
     return (
       <div>
         <svg className="bracket-svg" width={round*244} height={match*108} viewBox={`-0 0 ${round*244} ${match*108}`}>
@@ -260,6 +321,7 @@ class Test extends React.PureComponent {
             </g>
 
             <g>
+              {this.connector()}
               {this.bracket()}
             </g>
           </g>
